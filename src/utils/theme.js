c8f949eacf000c4ff08029c2bccce2f9bdf0a418
hosam salah <hosamsalah8081@gmail.com>
2023-07-23 11:41:44 +0300

    update theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -6,5 +6,11 @@ export const theme = createTheme({
         main: "#90B400",
         light: "white",
       },
+      danger: {
+        main:"#EC0808"
+      },
+      dark: {
+        main:"#000000"
+      }
     },
   });
